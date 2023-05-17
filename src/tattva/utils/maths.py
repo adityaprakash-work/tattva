9287f4eefd41be6f230a5ea18adf4aeaa3a17be0
adityaprakash-work <adityaprakash.work@gmail.com>
2023-05-17 05:55:15 +0530

    commit : 2

--- src/tattva/utils/maths.py
+++ src/tattva/utils/maths.py
@@ -1,13 +1,13 @@
-#---DEPENDENCIES---------------------------------------------------------------
+# ---DEPENDENCIES---------------------------------------------------------------
 import jax.numpy as jnp
 from jax import jit
 
 
-#---FUNCTIONS------------------------------------------------------------------
+# ---FUNCTIONS------------------------------------------------------------------
 @jit
-def hardclip(input_array: jnp.array, set_min: float = 0., set_max: float = 1.):
+def hardclip(input_array: jnp.array, set_min: float = 0.0, set_max: float = 1.0):
     """
-    Clips the values in the input array to lie between the set_min and set_max 
+    Clips the values in the input array to lie between the set_min and set_max
     values.
 
     Args:
@@ -16,15 +16,15 @@ def hardclip(input_array: jnp.array, set_min: float = 0., set_max: float = 1.):
         set_max: The maximum value to clip the array to. Defaults to 1.
 
     Returns:
-        A JAX array of the same shape as the input_array, with values 
+        A JAX array of the same shape as the input_array, with values
         clipped to the set_min and set_max range.
 
-    The hard clip function is a simple way to "clamp" the values in an array to 
+    The hard clip function is a simple way to "clamp" the values in an array to
     a specific range. Values below set_min will be set to set_min, and values
-    above set_max will be set to set_max. Values within the set_min and set_max 
+    above set_max will be set to set_max. Values within the set_min and set_max
     range will be left unchanged.
     """
-    return jnp.clip(input_array, a_min = set_min, a_max = set_max)
+    return jnp.clip(input_array, a_min=set_min, a_max=set_max)
 
 
 @jit
@@ -36,7 +36,7 @@ def softclip(input_array: jnp.array):
         input_array: A JAX array of numerical values.
 
     Returns:
-        A NumPy or JAX array of the same shape as the input_array, with values 
+        A NumPy or JAX array of the same shape as the input_array, with values
         transformed by the soft clip function.
 
     The soft clip function is defined as:
@@ -44,10 +44,8 @@ def softclip(input_array: jnp.array):
         y = 1 / (1 + e^(-4(x - 0.5))) -- clips to range (0, 1)
 
     where x is the input value, and y is the transformed value. This function
-    has the effect of compressing extreme values towards the center of the 
+    has the effect of compressing extreme values towards the center of the
     range, while leaving moderate values relatively unchanged.
     """
-    
+
     return 1 / (1 + jnp.exp(-4 * (input_array - 0.5)))
-
-
